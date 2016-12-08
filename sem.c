984a2df048a3db8e7be16b6ca7afe64d7fc9ce8f
esandine <esandine@stuy.edu>
2016-12-08 10:54:45 -0500

    modulized create

--- sem.c
+++ sem.c
@@ -4,6 +4,7 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <string.h>
+#include <sys/shm.h>
 
 union semun{
   int val;
@@ -12,18 +13,40 @@ union semun{
   struct seminfo *_buf;
 };
 
+int createSemaphore(int* sc){
+  int key = ftok("makefile", 22);
+  int semid;
+  //Creates Semaphore
+  semid = semget(key, 1, IPC_CREAT | 0644);
+  printf("semaphore created %d\n", semid);
+  union semun su;
+  su.val = 10;
+  //setting semaphore value
+  *sc = semctl(semid, 0, SETVAL, su);
+  printf("value set: %d\n", *sc);
+  return semid;
+}
+
+int createShmem(int *sh){
+  int key = ftok("makefile", 22);
+  int shmid;
+  //Creates Shmem
+  shmid = shmget(key, 4, IPC_CREAT | 0644);
+  printf("shmem created %d\n", shmid);
+  //setting shmemaphore value
+  sh = (int*)shmat(shmid, 0, 0);
+  printf("mem attatched: %d\n", *sh);
+  return shmid;
+}
 int main(int argc, char *argv[]){
   int semid;
-  int key = ftok("makefile" , 22);
+  int shmid;
   int sc;
+  int *sh;
+  int key = ftok("makefile", 22);
   if (strncmp(argv[1], "-c", strlen(argv[1])) == 0){
-    semid = semget(key, 1, IPC_CREAT | 0644);
-    printf("semaphore createdL %d\n", semid);
-    union semun su;
-    su.val = 1;
-    //setting semaphore value
-    sc = semctl(semid, 0, SETVAL, su);
-    printf("value set: %d\n", sc);
+    semid = createSemaphore(&sc);
+    shmid = createShmem(sh);
   }
   else if (strncmp(argv[1], "-v", strlen(argv[1])) == 0){
     semid = semget(key, 1, 0);
